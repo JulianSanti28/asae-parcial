f329544fad79be69638eca7979b81cc26e784741
Dacoes <dacoes@unicauca.edu.co>
2022-12-20 17:41:47 -0500

    @Querys student and teacher

--- parcial/src/main/java/co/unicauca/parcial/dao/IStudentRepository.java
+++ parcial/src/main/java/co/unicauca/parcial/dao/IStudentRepository.java
@@ -1,9 +1,23 @@
 package co.unicauca.parcial.dao;
 
 import co.unicauca.parcial.model.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IStudentRepository extends CrudRepository<Student,Integer> {
+    //Mediante la notación @Query consultar si existe un estudiante con un correo electrónico.
+    //Utilizar la consulta en la validación correspondiente
+    @Query("Select e From student where e.email = :email")
+    public Student buscarEstudiantePorCorreo(@Param("email") String email);
+
+    //Mediante la notación @Query consultar si existe un estudiante a partir de su número y tipo
+    //de identificación. Utilizar la consulta en la validación correspondiente.
+    @Query("select e from Student where e.identificationNumber = :identificationNumber and e.identificationType = :identificationType")
+    public Student buscarEstudiantePorNumeroYTipoIdentificacion(@Param("identificationNumber") String identificationNumber,
+                                                                @Param("identificationType") String identificationType);
+
+
 }
